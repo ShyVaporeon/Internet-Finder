28ecc0fbb243cbffe2c2cfb25ebc9427df548c9c
ShyVaporeon <shyvaporeon@gmail.com>
2021-09-23 01:07:55 +0300

    reformatted puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -1,19 +1,52 @@
 const puppeteer = require('puppeteer');
 
+async function findInternet(){
 
-async function Start_Browser() {
+	//startBrowser();
+
+	const url = "https://giris.turktelekomwifi.com/#/welcomeAdsl";
+	
 	const browser = await puppeteer.launch({
-		headless: false,
+		headless: true,
 		defaultViewport: null
 	});
 
 	const page = await browser.newPage();
-	
-	const url = "https://giris.turktelekomwifi.com/#/welcomeAdsl";
 
 	await page.goto(url,{
 		waitUntil: "domcontentloaded"
-	});	
+	});
+
+	//navigateSite(page);
+
+	//fillForm(page);	
+
+	showQuota(page);
+
+	//exitAccount(page);
+
+}
+
+/* async function startBrowser(){
+
+} */
+
+
+async function navigateSite(page){
+
+}
+
+async function fillForm(page){
+
+}
+
+async function exitAccount(page){
+	const exitButton = "data-autamation=\"cikis\"";
+
+}
+
+
+async function showQuota(page) {
 
 	const quota_remainging = ".table tbody tr td:first-child"
 	const quota_total = ".table tbody tr td:nth-child(2)"
@@ -21,11 +54,10 @@ async function Start_Browser() {
 		visible: true,
 	  });
 
-
 	const Message1 =  await page.$eval(quota_total, result => result.textContent);
 	const Message2 =  await page.$eval(quota_remainging, result => result.textContent);
 	console.log("Quota: "+Message1);
 	console.log("Remaining: "+Message2);
 }
 
-Start_Browser();
+findInternet();
